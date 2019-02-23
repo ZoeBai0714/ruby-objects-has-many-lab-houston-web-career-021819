fd4a86d45d482640fc1060b4573badd13f7e3cb7
Zoe Bai <s.xue.bai@gmail.com>
2019-02-23 21:10:44 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Author
+  attr_accessor :name, :posts
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def
+  
+  
+  def add_post(post)
+    @posts << post  #add under current author
+   post.author = self
+   Post.all << post ##add under all the posts
+  end
+  
+  def add_post_by_title(post_title)
+     new_post = Post.new(post_title)
+     new_post.author = self
+     @posts << new_post
+  end
+  
+  def self.post_count
+    Post.all.length
+    #self.posts.length
+  end
+  
+end
